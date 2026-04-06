1e027bc3d49f66e7db89a15c60e8c50ac60b83ad
SengQT <kyexd16@gmail.com>
2026-04-06 17:23:00 +0700

    mkdir media add .mp3 wrote extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,28 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// This is the player that talks to your Pop!_OS speakers
+const player = require('play-sound')({});
+
 export function activate(context: vscode.ExtensionContext) {
+    console.log('AoooTerminalErrorSound is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "AoooTerminalErrorSound" is now active!');
+    // This registers the command name
+    let disposable = vscode.commands.registerCommand('aooo.chowayo', () => {
+        
+        // Find the sound file inside your extension's media folder
+        const soundPath = path.join(context.extensionPath, 'media', 'chowayo.mp3');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('AoooTerminalErrorSound.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ChowayoAwwwww!');
-	});
+        // Play the sound!
+        player.play(soundPath, (err: any) => {
+            if (err) {
+                vscode.window.showErrorMessage("Chowayo sound error: " + err);
+            }
+        });
 
-	context.subscriptions.push(disposable);
+        // Show a "Chowayo" message in the bottom right corner
+        vscode.window.showInformationMessage('CHOWAYO! 🎃');
+    });
+
+    context.subscriptions.push(disposable);
 }
-
-// This method is called when your extension is deactivated
-export function deactivate() {}
